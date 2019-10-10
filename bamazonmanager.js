1b5f0e92b24c65609feb8f1af0b833224591fb81
Zaya <zayazaw67@gmail.com>
2019-10-09 23:23:19 -0500

    fixed addstock syntax (had wrong order), added gifs

--- bamazonmanager.js
+++ bamazonmanager.js
@@ -8,7 +8,6 @@ const connection = mysql.createConnection({
     password: "root",
     database: "bAmazon"
 });
-managerTools();
 
 function managerTools() {
     inquirer.prompt([
@@ -42,8 +41,6 @@ function viewItems() {
 
 function lowStock() {
     connection.query("SELECT * FROM items", function (err, response) {
-        // console.table(response)
-        // console.log(response)
         for (let i = 0; i < response.length; i++) {
             if (response[i].Stock <= 5) {
                 console.table(response[i])
@@ -73,18 +70,17 @@ function addStock() {
     ]).then(function (response) {
         let countWanted = response.addCount;
         let itemSelected = response.add;
-        databaseAdd(countWanted, itemSelected);
+        databaseAdd(itemSelected, countWanted);
     });
 };
 
 function databaseAdd(item, amount) {
     connection.query(`SELECT * FROM items WHERE id = '${item}'`, function (error, response) {
-        // console.table(response)
         if (error) { throw error }
         else {
             connection.query(`UPDATE items SET Stock = Stock + ${amount} WHERE id = '${item}'`)
             connection.query(`SELECT * FROM items WHERE id = '${item}'`, function (error, response) {
-                console.log("There are now " + response[0].Stock + " " + response[0].ItemName + ".")
+                console.log("Items have been added to our stock. There are now " + response[0].Stock + " " + response[0].ItemName + ".")
             });
         };
         connection.end();
@@ -104,7 +100,6 @@ function addItem() {
         {
             message: "How much should this item be sold for?",
             name: "price",
-            // probably won't work if there's a decimal?
             validate: function (value) {
                 if (isNaN(value) === false) { return true }
             }
@@ -125,21 +120,10 @@ function addItem() {
         }),
         function(err, res) {
             if (err) { throw err };
-            console.log("item added")
         };
-        connection.end();
+        console.log("New item has been added! Here is our updated store.")
+        viewItems();
     });
 }
 
-//         connection.query("UPDATE items SET ? WHERE ?", [
-//             {
-//                 stock: response.addCount
-//             },
-//             {
-//                 id: response.add
-//             },
-//         ]), function(err, data) {
-//             console.log(data)
-//         };
-//     });
-// };
+managerTools();
